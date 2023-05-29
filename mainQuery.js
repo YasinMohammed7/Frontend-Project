eb0e214fddccf0e8d5c7219eb39ef33d14f8b4c8
YasinMohammed7 <mohammedyasinym16720@gmail.com>
2023-05-30 00:08:36 +0300

    project commit

--- mainQuery.js
+++ mainQuery.js
@@ -19,7 +19,6 @@ $(document).ready(function () {
             containerSearch.slideToggle('fast')
         })
 
-
     }
 
     function hideSearch() {
@@ -31,11 +30,60 @@ $(document).ready(function () {
 
     }
 
+    // footer 
+
+    function arrow1() {
+
+
+        $('.arrow-down1').click(() => {
+            $('.footer-col1').slideToggle()
+            $('.arrow-down1').css("transition", "0.3s ease")
+
+
+            if ($('.arrow-down1').css("transform") === "none") {
+
+                $('.arrow-down1').css("transform", "rotateZ(-180deg)");
+            } else {
+                $('.arrow-down1').css("transform", "none");
+            }
+
+
+        })
+    }
+
+    function arrow2() {
+        $('.footer-col2').hide()
+
+        $('.arrow-down2').click(() => {
+            $('.footer-col2').slideToggle()
+            $('.arrow-down2').css("transition", "0.3s ease")
+
+            if ($('.arrow-down2').css("transform") === "none") {
+
+                $('.arrow-down2').css("transform", "rotateZ(-180deg)");
+
+
+            } else {
+                $('.arrow-down2').css("transform", "none");
+            }
+
+
+        })
+
+    }
+
+    arrow1()
+    arrow2()
+
     function handleMediaQuery(event) {
         if (event.matches) {
             showSearch()
+            $('.footer-col1').hide()
+            $('.footer-col2').hide()
         } else {
             hideSearch()
+            $('.footer-col1').show()
+            $('.footer-col2').show()
         }
     }
 
@@ -54,31 +102,33 @@ $(document).ready(function () {
         $('.burger-menu').animate({ width: 'toggle' })
     })
 
-    // footer 
-
-    $('.arrow-down1').click(() => {
-        if ($('.arrow-down1').css("transform") === "none") {
-
-            $('.arrow-down1').css("transform", "rotateZ(-180deg)");
-        } else {
-            $('.arrow-down1').css("transform", "none");
-        }
-
-        $('.arrow-down1').css("transition", "0.5s")
-        $('.footer-col1').slideToggle()
-    })
-
-    $('.arrow-down2').click(() => {
-        if ($('.arrow-down2').css("transform") === "none") {
-
-            $('.arrow-down2').css("transform", "rotateZ(-180deg)");
-        } else {
-            $('.arrow-down2').css("transform", "none");
-        }
-
-        $('.arrow-down2').css("transition", "0.5s")
-        $('.footer-col2').slideToggle()
-    })
-
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // function hideArrow1() {
+    //     $('.footer-col1').show()
+
+    // }
+
+    // function hideArrow2() {
+    //     $('.footer-col2').show()
+    // }
